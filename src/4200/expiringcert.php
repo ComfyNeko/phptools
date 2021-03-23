5080f23371ec010f33f26583f26f926e76d9c422
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-23 17:32:26 +0530

    Update expiringcert.php

--- src/4200/expiringcert.php
+++ src/4200/expiringcert.php
@@ -4,8 +4,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong&effect=emboss">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<head> 
-	<title>Insert Page page</title> 
+<head>
 </head> 
 <style> 
 body {
